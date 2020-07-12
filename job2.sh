32aa3e010d4f73497564cab2405f3332f903e7c9
Chirag Chaudhuri <chiragchaudhuri02@gmail.com>
2020-07-12 18:44:41 +0530

    Update job2.sh

--- job2.sh
+++ job2.sh
@@ -1,8 +1,8 @@
-if sudo kubectl get all --kubeconfig /root/.kube/config |grep webtest
+if sudo /usr/local/bin/kubectl get all --kubeconfig /root/.kube/config |grep webtest
 then
-sudo kubectl delete all --all --kubeconfig /root/.kube/config 
-sudo kubectl delete pvc --all --kubeconfig /root/.kube/config 
-sudo kubectl create -k . --kubeconfig /root/.kube/config 
+sudo /usr/local/bin/kubectl delete all --all --kubeconfig /root/.kube/config 
+sudo /usr/local/bin/kubectl delete pvc --all --kubeconfig /root/.kube/config 
+sudo /usr/local/bin/kubectl create -k . --kubeconfig /root/.kube/config 
 else
-sudo kubectl create -k . --kubeconfig /root/.kube/config 
+sudo /usr/local/bin/kubectl create -k . --kubeconfig /root/.kube/config 
 fi
