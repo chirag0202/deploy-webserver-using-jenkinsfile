94fdf2467ab82d7a9d2544d3720145ef858170b9
Chirag Chaudhuri <chiragchaudhuri02@gmail.com>
2020-07-12 18:54:07 +0530

    Update job2-2.sh

--- job2-2.sh
+++ job2-2.sh
@@ -1,3 +1,3 @@
 sleep 15
-sudo /usr/local/bin/kubectl cp /task6/* $(sudo /usr/local/bin/kubectl get pod --kubeconfig /root/.kube/config|grep webtest| awk '{print $1'):/var/www/html --kubeconfig /root/.kube/config 
+sudo /usr/local/bin/kubectl cp /task6/* $(sudo /usr/local/bin/kubectl get pod --kubeconfig /root/.kube/config|grep webtest| awk '{print $1}'):/var/www/html --kubeconfig /root/.kube/config 
 sudo /usr/local/bin/kubectl get all --kubeconfig /root/.kube/config 
